f8f18ae3a3469c15296601b5696c2a922b0f7d83
hacKING-25 <dabadekunal69@gmail.com>
2025-11-23 23:20:23 +0530

    q5

--- ground3.js
+++ ground3.js
@@ -2,7 +2,7 @@
 // Create a function that checks if a given password meets the following criteria: at least 8 characters long, 
 // contains both uppercase and lowercase letters,and includes at least one digit.
 
-function validatePassword(password) {
+/*function validatePassword(password) {
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
     const hasDigit = /\d/.test(password);
@@ -14,5 +14,26 @@ function validatePassword(password) {
 // Example usage:
 const password1 = "NIcePass123";
 const password2 = "hellno";
-console.log(validatePassword(password1)); // true
-console.log(validatePassword(password2)); // false
+console.log(validatePassword(password1)); 
+console.log(validatePassword(password2)); 
+*/
+
+
+// The Sum selector: You are working on function that should sum all the numbers in an array until it encounters 
+// a negative number. Write a function that implements this logic.
+
+function sumUntilNegative(arr) {
+    let sum = 0;
+    for (let num of arr) {
+        if (num < 0) {
+            break;
+        }
+        sum += num;
+    }
+    return sum;
+}
+
+// Example usage:
+const numbers = [5, -10, -15, -3, 20];
+const totalSum = sumUntilNegative(numbers);
+console.log(totalSum);
