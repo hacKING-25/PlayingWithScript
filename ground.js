69de9e260c6af5327df438c5400758efdc96a1d9
hacKING-25 <dabadekunal69@gmail.com>
2025-11-23 22:51:05 +0530

    q2

--- ground.js
+++ ground.js
@@ -4,32 +4,33 @@
 // Hufflepuff(length less than 8), Ravenclaw(length less than 12), 
 // or Slytherin(length greater than or equal to 12)) based on the length of their names.
 
-function sortStudentsIntoHouses(studentNames) {
-    const houses = {
-        Gryffindor: [],
-        Hufflepuff: [],
-        Ravenclaw: [],
-        Slytherin: []
-    };
+ function sortStudentsIntoHouses(studentNames) {
+     const houses = {
+         Gryffindor: [],
+         Hufflepuff: [],
+         Ravenclaw: [],
+         Slytherin: []
+     };
 
-    studentNames.forEach(name => {
-        const nameLength = name.length;
+   studentNames.forEach(name => {
+         const nameLength = name.length;
 
-        if (nameLength < 6) {
-            houses.Gryffindor.push(name);
-        } else if (nameLength < 8) {
-            houses.Hufflepuff.push(name);
-        } else if (nameLength < 12) {
+      if (nameLength < 6) {
+             houses.Gryffindor.push(name);
+         } else if (nameLength < 8) {
+             houses.Hufflepuff.push(name);
+    } else if (nameLength < 12) {
             houses.Ravenclaw.push(name);
         } else {
-            houses.Slytherin.push(name);
-        }
-    });
+             houses.Slytherin.push(name);
+         }
+     });
 
-    return houses;
-}
+     return houses;
+ }
 
-// Example usage:
-const students = ["Harry", "Hermione", "Ron", "Luna", "DracoMalfoy", "NevilleLongbottom","Panther"];
-const sortedHouses = sortStudentsIntoHouses(students);
-console.log(sortedHouses);
+ // Example usage:
+ const students = ["Harry", "Hermione", "Ron", "Luna", "DracoMalfoy", "NevilleLongbottom","Panther"];
+ const sortedHouses = sortStudentsIntoHouses(students);
+ console.log(sortedHouses);
+
